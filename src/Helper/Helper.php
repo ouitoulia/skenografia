9cf6aeb280966f4c5562836137ea32d5f15f900a
arturu <arturu@arturu.it>
2025-01-24 17:21:24 +0100

    release(2.30.0): add dirigenti e tassi di assenza

--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -53,4 +53,64 @@ class Helper {
     return $data;
   }
 
+  /**
+   * @param $opzioni array
+   *   - "formato_anno" ("yyyy-yyyy", "yyyy-yy", "yy-yy")
+   *      il formato di output dell'anno: 4 o 2 cifre. Default: "yyyy-yyyy"
+   *   - "separatore" ("/", " ", "")
+   *      il separatore da usare, max un carattere. Default: ""
+   *   - "periodo" ("now", "prev", "next")
+   *      il periodo da calcolare. Default: "now"
+   *
+   * @return string
+   *   stringa con l'anno scolastico in base al formato
+   *     - "2020/2021", "2020/21", "20/21"
+   *     - "2020-2021", "2020-21", "20-21"
+   *     - "2020 2021", "2020 21", "20 21"
+   *     - "20202021", "202021", "2021"
+   */
+  public static function getAnnoScolastico(array $opzioni = NULL): string {
+    // Imposto i valori di default
+    $formato_anno = $opzioni['formato_anno'] ?? 'yyyy-yyyy';
+    $separatore = $opzioni['separatore'] ?? '/';
+    $periodo = $opzioni['periodo'] ?? 'now';
+
+    // Data corrente
+    $mese_corrente = (int) date('n'); // Mese (1-12)
+    $giorno_corrente = (int) date('j'); // Giorno (1-31)
+
+    // Data corrente espresso in cifre: 31 Agosto = 831
+    $data_corrente = ($mese_corrente * 100) + $giorno_corrente;
+
+    // La parte che sta a sinistra dell'AS
+    $parte_sinistra = (int) date('Y');
+
+    // In caso bisogna calcolare l'AS precedente
+    if ($periodo === 'prev') {
+      $parte_sinistra--;
+    // In caso bisogna calcolare l'AS successivo
+    } elseif ($periodo === 'next') {
+      $parte_sinistra++;
+    }
+
+    if ($data_corrente <= 831) {
+      $parte_sinistra--; // Prima del 31 agosto, l'anno scolastico è quello precedente
+    }
+
+    // Calcola la seconda parte dell'AS
+    $parte_destra = $parte_sinistra + 1;
+
+    // Formatta gli anni in base al formato specificato
+    $parte_sinistra_formattata = ($formato_anno === 'yy-yy')
+      ? substr((string) $parte_sinistra, -2)
+      : (string) $parte_sinistra;
+
+    $parte_destra_formattata = ($formato_anno === 'yy-yy' || $formato_anno === 'yyyy-yy')
+      ? substr((string) $parte_destra, -2)
+      : (string) $parte_destra;
+
+    // Costruisce la stringa finale
+    return $parte_sinistra_formattata . $separatore . $parte_destra_formattata;
+  }
+
 }
